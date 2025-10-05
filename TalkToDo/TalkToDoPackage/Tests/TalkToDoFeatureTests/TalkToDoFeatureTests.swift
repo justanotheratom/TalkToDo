81084178c4ad6d3fd7e15c9787404a5e0104b60f
justanotheratom <sanketpatel.1805@gmail.com>
2025-10-05 23:23:22 +0530

    Fix build issues: Swift 6.0 tools version and add test target

---
+++ TalkToDo/TalkToDoPackage/Tests/TalkToDoFeatureTests/TalkToDoFeatureTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import TalkToDoFeature
+
+final class TalkToDoFeatureTests: XCTestCase {
+    func testExample() throws {
+        // Placeholder test
+        XCTAssertTrue(true)
+    }
+}
